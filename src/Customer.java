542d04061ed75c1b7db855b992d3f41ddd8785dc
Nico Diefenbacher <nicodiefenbacher@Nicos-MBP.fritz.box>
2020-05-19 10:45:06 +0200

    Move and rename function amountFor(..) to getCharge() from Customer.java to Rental.java

--- src/Customer.java
+++ src/Customer.java
@@ -39,27 +39,7 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) throws Exception {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
-            default:
-                throw new Exception("unexpected PriceCode");
-        }
-        return thisAmount;
-    }
+
 
     private int getFrequentRenterPoints(Rental each) {
         // set frequent renter point
